c63369f0fb1d15b3b9aac50da7e18235810595d9
boweiww <boweiww@gmail.com>
2019-03-10 19:19:46 -0600

    3/10

---
+++ VideoHandler.py
@@ -0,0 +1,56 @@
+import cv2
+import os
+
+# Beach Aerial Footage Taken by a Drone.mp4
+class ToFrames():
+
+    def __init__(self, filename):
+        print("opencv version: "+cv2.__version__)
+        self.vidcap = cv2.VideoCapture(filename)
+
+        # imagesize is 800*450*3
+        self.success, self.image = self.vidcap.read()
+        # count = 0
+        # success = True
+        # foldername = filename.split('.')[0]
+        # if not os.path.exists(foldername):
+        #     os.makedirs(foldername)
+        # cwd = os.getcwd()
+        # directory = cwd + '\\' + foldername
+        # while success:
+        #     completeName = os.path.join(directory, "frame%d.jpg" % count)
+        #     cv2.imwrite(completeName, image)  # save frame as JPEG file
+        #     success, image = vidcap.read()
+        #     count += 1
+        # print("%d frames are generated" % count)
+    def GetCurrent(self):
+        return self.image
+
+    def GetNext(self):
+        self.success, self.image = self.vidcap.read()
+        if self.success:
+            return self.image
+        else:
+            return False
+
+# def ToFrames (filename):
+#         print("opencv version: "+cv2.__version__)
+#         vidcap = cv2.VideoCapture(filename)
+#
+#         # imagesize is 800*450*3
+#         success, image = vidcap.read()
+#         count = 0
+#         success = True
+#         foldername = filename.split('.')[0]
+#         if not os.path.exists(foldername):
+#             os.makedirs(foldername)
+#         cwd = os.getcwd()
+#         directory = cwd + '\\' + foldername
+#         while success:
+#             completeName = os.path.join(directory, "frame%d.jpg" % count)
+#             cv2.imwrite(completeName, image)  # save frame as JPEG file
+#             success, image = vidcap.read()
+#             count += 1
+#         print("%d frames are generated" % count)
+# if __name__=="__main__":
+#     ToFrames('Beach Aerial Footage Taken by a Drone.mp4')
--- video-segmentation.py
+++ video-segmentation.py
@@ -1,5 +1,52 @@
 # video from
 # https://videos.pexels.com/videos/beach-aerial-footage-taken-by-a-drone-854218
-def main():
-    for i in range(numframes):
-        image = getframes(i)
+from VideoHandler import ToFrames
+import numpy as np
+import matplotlib.pyplot as plt
+Threshold = 0.65
+def videoSegment(filename):
+    frames = ToFrames(filename)
+    prev = frames.GetCurrent()
+    image = frames.GetNext()
+    sub = []
+    total = []
+    while(type(image) == np.ndarray):
+        total.append(int(image.sum()))
+        dif = np.absolute(prev - image)
+        sub.append(int(dif.sum()))
+        prev = image
+        image = frames.GetNext()
+    sublen = len(sub)
+    numframes = 1
+    avg = sub[0]
+    for i in range(1,sublen):
+        # difference = abs(sub[i] - sub[i-1])
+        difference = abs(sub[i] - avg)
+        numframes = numframes+1
+        per = 1/numframes
+        avg = avg*(1-per) + per*sub[i]
+        if difference/avg>Threshold:
+            print(i)
+            print(difference/avg)
+            numframes = 1
+            avg = sub[i]
+        if i == 281:
+            print(i)
+            print(difference / avg)
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(sub)
+    plt.ylabel('difference')
+    plt.xlabel('frames')
+
+    # plt.show()
+    plt.subplot(212)
+
+    plt.plot(total)
+    plt.ylabel('values')
+    plt.xlabel('frames')
+
+    plt.show()
+
+if __name__=="__main__":
+    videoSegment('Beach Aerial Footage Taken by a Drone.mp4')
